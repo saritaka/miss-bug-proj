36b827c6ddf89b8cb80d8971d64a5ad7243cc5d7
saritaka <akalesarit@gmail.com>
2024-04-29 16:47:18 +0300

    added cookies

--- node-express-server/server.js
+++ node-express-server/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 import { bugService } from "./Services/bug.service.js";
 import { loggerService } from "./Services/logger.service.js";
@@ -19,6 +20,7 @@ const corsOptions = {
 
 app.use(express.static("public"));
 app.use(cors(corsOptions));
+app.use(cookieParser());
 
 app.listen(3030, () => console.log("Server ready at port 3030"));
 
@@ -66,12 +68,40 @@ app.get("/api/bug/:bugId", async (req, res) => {
   try {
     const id = req.params.bugId;
     const bug = await bugService.getById(id);
-    res.send(bug);
+    let visitedCount = req.cookies.visitedCount || [];
+
+    var add = true;
+    for (var index = 0; index < visitedCount.length; ++index) {
+      var _bug = visitedCount[index];
+      if (_bug._id === id) {
+        add = false;
+      }
+    }
+    if (add) {
+      visitedCount.push(bug);
+    }
+    res.cookie("visitedCount", visitedCount, {
+      maxAge: 7 * 1000,
+    });
+    if (visitedCount.length > 3) {
+      res.status(401).send("Wait for a bit");
+    } else {
+      res.send(bug);
+    }
   } catch (error) {
     loggerService.error(`Couldn't get bug`, error);
     res.status(400).send(`Couldn't get bug`);
   }
 });
+
+// app.get("/api/bug/:bugId", (req, res) => {
+//   let visitedCount = req.cookies.visitedCount || 0;
+//   res.cookie("visitedCount", ++visitedCount, { maxAge: 7 * 1000 });
+//   //   res.send(`<h1>Visited ${visitedCount} times</h1>`);
+//   if (visitedCount > 3) {
+//     res.status(401).send("Wait for a bit");
+//   }
+// });
 
 // app.get("/api/logs", async (req, res) => {
 //   res.sendFile(process.cwd() + "/logs/backend.log");
